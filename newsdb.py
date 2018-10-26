6c1c4e654fa468417033412eccd2d10476c3632f
Denis Gonçalves Marques <denwork@outlook.com>
2018-10-26 01:44:32 -0300

    format: Add shebang python3

--- newsdb.py
+++ newsdb.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 import psycopg2
 from string import Template
 
@@ -14,33 +16,33 @@ def get_query_result(query):
     return result
 
 
-def get_top_articles_by_num_views():
-    results = get_query_result("SELECT * FROM " +
-                               "view_top_article_by_views")
+def get_top3_articles_by_num_views():
+    results = get_query_result("SELECT * FROM view_top3_article_by_views")
+
     articles = []
     for article, views in results:
-        articles.append('%s - %s' % (article, views,))
+        articles.append('%s - %s views' % (article, views,))
 
     return articles
 
 
 def get_top_authors_by_num_views_articles():
-    results = get_query_result("SELECT * FROM " +
-                               "view_top_authors_by_views_article")
-    authors = []
+    results = get_query_result(
+        "SELECT * FROM view_top_authors_by_views_article")
+
+    articles = []
     for author, views in results:
-        authors.append('%s - %s' % (author, views,))
+        articles.append('%s - %s views' % (author, views,))
 
-    return authors
+    return articles
 
 
-def get_errors_request_by_day_more_than_1percent():
-    """ Get HTTP request errors (4xx) by day if more then 1 percent of total fo requests"""
-    results = get_query_result("SELECT * FROM " +
-                               "view_errors_request_by_day_more_than_1percent")
+def get_error_request_by_day_more_than_1percent():
+    results = get_query_result(
+        "SELECT * FROM view_errors_request_by_day_more_than_1percent")
 
     errors = []
     for date, percent in results:
-        errors.append('%s - %s' % (date, percent,))
+        errors.append('%s - %s errors' % (date, percent,))
 
     return errors
